d724c73dabb2838ebe6dd772da3bd27d96e9acb3
tpk256 <tpk256@mail.ru>
2024-11-20 14:51:21 +0500

    create getting keyboard inline:

--- src/ESP8266_TG.ino
+++ src/ESP8266_TG.ino
@@ -1,8 +1,36 @@
 #include <FastBot2.h>
-#include <string>
+#include <cstring>
 
 FastBot2 bot;
 int64_t chat_id;
+const char* SSID = "tps";
+const char* PASSWORD = "2743113077"; 
+
+
+void update_get_keyboard(fb::Update& u){
+  fb::Message msg;
+  fb::InlineMenu menu;
+  msg.chatID = u.message().chat().id();
+
+  if ( strcmp(u.message().text().c_str(), "/menu") == 0){
+    msg.text = "Open the door?\0";
+    menu.addButton("Yes", "open_door_yes");
+    menu.addButton("No", "open_door_no");
+    msg.setInlineMenu(menu);
+  }
+  else{
+    msg.text = "I can't understand this command, use please /menu\0";
+  }
+  bot.sendMessage(msg);
+
+}
+
+
+void update_open_door(fb::Update& u){
+
+}
+
+
 void update (fb::Update& u){
     // Добавляем конкретный список userov tg (id)
 
@@ -10,19 +38,30 @@ void update (fb::Update& u){
     // Нужно inline конпку сделать,
     // когда прилетает да, то мы посылаем сигнал по пину и после отправляет измененное сообщение
     // что открыли или просто новое отправляем
-    chat_id = u.message().from().id();
-    fb::Message msg;
-    msg.text = u.message().text().str();
-    msg.chatID = chat_id;
-    bot.sendMessage(msg);
-   
+    
+
+    // ЛОГИКА ПОСЫЛАЕТСЯ КОМАНДА МЕНЮ, А МЫ ИНЛАЙН КНОПКУ ОТКРЫИТЬ
+  
+    if (u.isQuery()){
+        update_open_door(u);
+     }
+    else if (u.isMessage()){
+        update_get_keyboard(u);
+    }
 }
 
 void setup() {
   Serial.begin(115200);
 
-  // Тут дополнить подключение к wifi
+  WiFi.begin(SSID,  PASSWORD);
 
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("Connected");
   bot.setToken("7282964831:AAFpXPLc0eODNVYyO5QNGbrzgDYIfKC3wGE");
   bot.skipUpdates(0);
   bot.setLimit(3);
